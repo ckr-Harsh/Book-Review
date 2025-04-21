7df58d4a49aa386590ed104f7bed1187b25e6268
Harsh Choudhary <harsh.chaudhary@appsentinels.ai>
2025-04-21 12:22:19 +0530

    minor changes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet, ReviewViewSet
+from . import views
 
 router = DefaultRouter()
-router.register(r'books', BookViewSet)
-router.register(r'reviews', ReviewViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('books', views.BookListAPIView.as_view(), name='book-list'),
+    path('books/<int:pk>', views.BookDetailAPIView.as_view(), name='book-detail'),
+    path('books/<int:book_id>/reviews', views.ReviewListCreateAPIView.as_view(), name='book-reviews'),
+    path('books/<int:book_id>/reviews/<int:pk>', views.ReviewRetrieveUpdateDestroyAPIView.as_view(), name='review-detail'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,33 @@
-from rest_framework import viewsets
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework import filters
 from .models import Book, Review
 from .serializers import BookSerializer, ReviewSerializer
 
-class BookViewSet(viewsets.ModelViewSet):
+class BookListAPIView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['genre', 'author']
+
+class BookDetailAPIView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class ReviewViewSet(viewsets.ModelViewSet):
-    queryset = Review.objects.all()
+class ReviewListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        book_id = self.kwargs['book_id']
+        return Review.objects.filter(book_id=book_id)
+
+    def perform_create(self, serializer):
+        book_id = self.kwargs['book_id']
+        serializer.save(book_id=book_id)
+
+class ReviewRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        book_id = self.kwargs['book_id']
+        return Review.objects.filter(book_id=book_id)
